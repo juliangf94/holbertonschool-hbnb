5c79003fc77c9592862390c4a3779388fdb9725b
Gigi-Corlay <11489@holbertonstudents.com>
2026-03-15 09:28:38 +0100

    Add to ForeignKey

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -3,6 +3,16 @@ from app.extensions import db
 from app.models.base_model import BaseModel
 
 
+"""
+Association table for many-to-many relationship
+"""
+place_amenity = db.Table(
+    "place_amenity",
+    db.Column("place_id", db.String(36), db.ForeignKey("places.id"), primary_key=True),
+    db.Column("amenity_id", db.String(36), db.ForeignKey("amenities.id"), primary_key=True)
+)
+
+
 class Place(BaseModel, db.Model):
     __tablename__ = "places"
 
@@ -12,6 +22,23 @@ class Place(BaseModel, db.Model):
     price = db.Column(db.Float, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
+
+    """
+    Foreign key
+    """
+    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+
+    """
+    Relationships
+    """
+    reviews = db.relationship('Review', backref='place', lazy=True)
+
+    amenities = db.relationship(
+        "Amenity",
+        secondary=place_amenity,
+        backref=db.backref("places", lazy=True),
+        lazy=True
+    )
 
     def __init__(self, title, price, latitude, longitude, description="", **kwargs):
         super().__init__(**kwargs)
